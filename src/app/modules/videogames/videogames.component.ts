2620cbf607cc8f7f86fa368d295a05eba1e71592
Minnu <shajiminnu17@gmail.com>
2021-04-21 11:44:46 +0530

    video games page initial version

--- src/app/modules/videogames/videogames.component.ts
+++ src/app/modules/videogames/videogames.component.ts
@@ -1,5 +1,5 @@
 import { AotSummaryResolver } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import AOS from 'aos';
 
 @Component({
@@ -9,10 +9,37 @@ import AOS from 'aos';
 })
 export class VideogamesComponent implements OnInit {
 
+  @ViewChild('witcher')witcher: ElementRef;
+  @ViewChild('nier')nier: ElementRef;
+  @ViewChild('dota')dota: ElementRef;
+  @ViewChild('tombraider')tombraider: ElementRef;
+  @ViewChild('cyberpunk')cyberpunk: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
     AOS.init();
+  }
+
+  scrollToElement(event) {
+    console.log('eleement ', event)
+    this.witcher.nativeElement.scrollIntoView({behavior: 'smooth'}); 
+  }
+
+  scrollToNier() {
+    this.nier.nativeElement.scrollIntoView({behavior: 'smooth'}); 
+  }
+
+  scrollToDota() {
+    this.dota.nativeElement.scrollIntoView({behavior: 'smooth'}); 
+  }
+
+  scrollToTombRaider() {
+    this.tombraider.nativeElement.scrollIntoView({behavior: 'smooth'}); 
+  }
+
+  scrollToCyberpunk() {
+    this.cyberpunk.nativeElement.scrollIntoView({behavior: 'smooth'}); 
   }
 
 }
